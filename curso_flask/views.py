da189a3b50544588d56bf5957e812dbf5e998e94
Fabio Klevinskas Lopes <fabioklopes@live.com>
2025-08-03 14:03:43 -0300

    Criando URL_FOR e passagem de variáveis para o HTML

--- curso_flask/views.py
+++ curso_flask/views.py
@@ -1,7 +1,20 @@
 from curso_flask import app
-from flask import render_template as render
-
+from flask import render_template as render, url_for
+    
 
 @app.route('/')
 def index():
-    return render('index.html')
+    usuario = "Fábio"
+    idade = 28
+
+    context = {
+        'usuario': usuario,
+        'idade': idade
+    }
+
+    return render('index.html', context=context)
+
+
+@app.route('/nova/')
+def novapagina():
+    return 'outra view'
